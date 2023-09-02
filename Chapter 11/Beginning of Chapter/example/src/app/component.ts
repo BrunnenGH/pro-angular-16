f93dadef42fed652f5bbd5490af9fa08a8fd8e56
Filip Ryjáček <115221413+filipryjacek-ext91476@users.noreply.github.com>
2023-09-02 12:53:26 +0200

    data binding

--- Chapter 11/Beginning of Chapter/example/src/app/component.ts
+++ Chapter 11/Beginning of Chapter/example/src/app/component.ts
@@ -8,11 +8,46 @@ import { Product } from "./product.model";
 })
 export class ProductComponent {
     private model: Model = new Model();
- 
+
     products = computed<Product[]>(() => this.model.Products());
 
     count = computed<number>(() => this.products().length);
 
-    classes = computed<string>(() => 
+    classes = computed<string>(() =>
         this.count() == 5 ? "bg-success" : "bg-warning");
+
+
+    /*
+    Podle ceny produktu vrátí css třídu bg-info nebobg-warning
+     */
+    getClasses(key: number) {
+        return "p-2 " + (((this.products()[key].price ?? 0) > 50)
+            ? "bg-info" : "bg-warning");
+    }
+
+    /*
+    Mapa pro [ngClass]
+     */
+    getClassMap(key: number): Object {
+        let product = this.products()[key];
+        return {
+            "text-center bg-danger": product.name == "Kayak",
+            "bg-info": (product.price ?? 0) < 50
+        };
+    }
+
+
+    fontSizeWithUnits: string = "30px";
+    fontSizeWithoutUnits: string= "30";
+
+    /*
+    [ngStyle]
+     */
+    getStyles(key: number) {
+        return {
+            fontSize: "30px",
+            "margin.px": 100,
+            color: (this.products()[key].price ?? 0) > 50 ? "red" : "green"
+        };
+    }
 }
